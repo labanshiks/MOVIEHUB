fd2d45805cb291a49a21428fb329b68dd8ea4cd7
labanshiks <labanshiks4@gmail.com>
2022-07-18 16:42:36 +0300

    Created Rest API User CRUD Operations

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(express.json());
 
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 
 
